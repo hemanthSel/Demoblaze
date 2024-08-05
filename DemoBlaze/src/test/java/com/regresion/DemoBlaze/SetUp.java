4daef106a31334aa2888c9e5c930c4f367103bd7
HemanthSel <hemanthdev22@gmail.com>
2024-08-05 19:40:26 -0400

    commit all updates in the folder structure

--- DemoBlaze/src/test/java/com/regresion/DemoBlaze/SetUp.java
+++ DemoBlaze/src/test/java/com/regresion/DemoBlaze/SetUp.java
@@ -19,8 +19,8 @@ import java.util.*;
 
 public class SetUp {
 	
-WebDriver driver;
-Properties prop = new Properties(); 
+public WebDriver driver;
+public Properties prop = new Properties(); 
 
 @BeforeClass
 public void Initialization() throws IOException
@@ -45,61 +45,5 @@ public void CleanUp()
 	
 }
 
-@Test
-public void Testmethod1() throws InterruptedException, IOException{
-	  FileInputStream excel = new FileInputStream(System.getProperty("user.dir")+"\\Books.xlsx");
-	  XSSFWorkbook wb = new XSSFWorkbook(excel); //Open Excel Sheet
-	  XSSFSheet sheet = wb.getSheet("Demoblaze");	
-	
-	
-	XSSFRow row = sheet.getRow(1);
-	XSSFCell col = row.getCell(1);
-	
-	XSSFRow row2 = sheet.getRow(1);
-	XSSFCell col2 = row2.getCell(1);
-	
-	String uName=col.getStringCellValue();
-	String uPwd=col2.getStringCellValue();
-	
-	wb.close();
-	
-	 String navlogin= prop.getProperty("login2");
-	 WebElement clickable = driver.findElement(By.id(navlogin));
-	 Actions action = new Actions(driver);
-	 action.click(clickable).perform();
-	 //action.release();
-	
-	 Thread.sleep(3000);
-	
-	 String usr= prop.getProperty("usrName");
-	 WebElement username=driver.findElement(By.id(usr));
-	 username.sendKeys(uName);
-	 
-	 String usrpwd = prop.getProperty("pwd");
-	 WebElement password=driver.findElement(By.id(usrpwd));
-	 password.sendKeys(uPwd); 
-	
-	 String loginBtnClick = prop.getProperty("btnLogin");
-	 WebElement login=driver.findElement(By.xpath(loginBtnClick));
-	 action.click(login).perform();
-	 //action.release();
-	 
-	 Thread.sleep(3000);
-	 String imgHyperlinkClick = prop.getProperty("ctnHyperLink");
-	 WebElement link = driver.findElement(By.linkText(imgHyperlinkClick));
-	 link.click();
-	 
-	 Thread.sleep(3000);
-	 String btnAddToCart = prop.getProperty("btnCart");
-	 WebElement add2cart = driver.findElement(By.linkText(btnAddToCart));
-	 add2cart.click();
-	 //action.release();
-	 
-	Alert alert= driver.switchTo().alert();
-	Thread.sleep(3000);
-	alert.accept();
-	
-
-}
 
 }
